a08afffab4ebeee8d3493ca3bf48b796d41919b7
Branislav Gerazov <gerazov@feit.ukim.edu.mk>
2019-07-18 14:50:05 +0200

    lecture materials v0.82 with EEGs

--- code/vezba2_eeg.py
+++ code/vezba2_eeg.py
@@ -80,106 +80,70 @@ b_alpha, a_alpha = sig.iirfilter(5, [8, 13], btype='band', fs=fs)
 b_beta, a_beta = sig.iirfilter(5, [13, 30], btype='band', fs=fs)
 b_gamma, a_gamma = sig.iirfilter(5, 30, btype='high', fs=fs)
 
+filter_bank = [
+        (b_delta, a_delta),
+        (b_theta, a_theta),
+        (b_alpha, a_alpha),
+        (b_beta, a_beta),
+        (b_gamma, a_gamma),
+        ]
+bands = 'delta theta alpha beta gamma'.split()
+
 # %% plot transfer
 plt.figure()
-f, h = sig.freqz(b_delta, a_delta, fs=fs)
-plt.plot(f, 20*np.log10(np.abs(h)))
-f, h = sig.freqz(b_theta, a_theta, fs=fs)
-plt.plot(f, 20*np.log10(np.abs(h)))
-f, h = sig.freqz(b_alpha, a_alpha, fs=fs)
-plt.plot(f, 20*np.log10(np.abs(h)))
-f, h = sig.freqz(b_beta, a_beta, fs=fs)
-plt.plot(f, 20*np.log10(np.abs(h)))
-f, h = sig.freqz(b_gamma, a_gamma, fs=fs)
-plt.plot(f, 20*np.log10(np.abs(h)))
+for (b, a), band in zip(filter_bank, bands):
+    f, h = sig.freqz(b, a, fs=fs)
+    plt.plot(f, 20*np.log10(np.abs(h)), lw=3, alpha=.7, label=band)
 plt.grid()
-plt.axis([0, 50, -80, 10])
-
-# %% filter
-eeg_open_delta = sig.lfilter(b_delta, a_delta, eeg_open_filt)
-eeg_closed_delta = sig.lfilter(b_delta, a_delta, eeg_closed_filt)
-
-eeg_open_theta = sig.lfilter(b_theta, a_theta, eeg_open_filt)
-eeg_closed_theta = sig.lfilter(b_theta, a_theta, eeg_closed_filt)
-
-eeg_open_alpha = sig.lfilter(b_alpha, a_alpha, eeg_open_filt)
-eeg_closed_alpha = sig.lfilter(b_alpha, a_alpha, eeg_closed_filt)
-
-eeg_open_beta = sig.lfilter(b_beta, a_beta, eeg_open_filt)
-eeg_closed_beta = sig.lfilter(b_beta, a_beta, eeg_closed_filt)
-
-eeg_open_gamma = sig.lfilter(b_gamma, a_gamma, eeg_open_filt)
-eeg_closed_gamma = sig.lfilter(b_gamma, a_gamma, eeg_closed_filt)
-
-# %% plot open
-plt.figure(figsize=(10, 10))
-plt.subplot(511)
-plt.plot(t, eeg_open_delta)
-plt.axis([10, 20, -1e-4, 1e-4])
-plt.subplot(512)
-plt.plot(t, eeg_open_theta)
-plt.axis([10, 20, -1e-4, 1e-4])
-plt.subplot(513)
-plt.plot(t, eeg_open_alpha)
-plt.axis([10, 20, -1e-4, 1e-4])
-plt.subplot(514)
-plt.plot(t, eeg_open_beta)
-plt.axis([10, 20, -1e-4, 1e-4])
-plt.subplot(515)
-plt.plot(t, eeg_open_gamma)
-plt.axis([10, 20, -1e-4, 1e-4])
+plt.legend()
+plt.axis([0, f[-1], -60, 5])
 plt.tight_layout()
 
-# %% plot closed
-eegs_closed = [
-        eeg_closed_delta,
-        eeg_closed_theta,
-        eeg_closed_alpha,
-        eeg_closed_beta,
-        eeg_closed_gamma,
-        ]
+# %% filter
+eeg_open_bands = []
+eeg_closed_bands = []
+for b, a in filter_bank:
+    eeg_open_bands.append(sig.lfilter(b, a, eeg_open_filt))
+    eeg_closed_bands.append(sig.lfilter(b, a, eeg_closed_filt))
+
+# %% plot bands open
 plt.figure(figsize=(10, 10))
-for i, eeg in enumerate(eegs_closed):
+for i, eeg in enumerate(eeg_open_bands):
+    plt.subplot(5, 1, i+1)
+    plt.plot(t, eeg)
+    plt.axis([10, 20, -1e-4, 1e-4])
+plt.tight_layout()
+
+# %% plot bands closed
+plt.figure(figsize=(10, 10))
+for i, eeg in enumerate(eeg_closed_bands):
     plt.subplot(5, 1, i+1)
     plt.plot(t, eeg)
     plt.axis([10, 20, -1e-4, 1e-4])
 plt.tight_layout()
 
 # %% calculate power
-eegs_open = [
-        eeg_open_delta,
-        eeg_open_theta,
-        eeg_open_alpha,
-        eeg_open_beta,
-        eeg_open_gamma,
-        ]
-eegs_closed = np.array(eegs_closed)
-eegs_open = np.array(eegs_open)
+eeg_open_bands = np.array(eeg_open_bands)
+eeg_closed_bands = np.array(eeg_closed_bands)
 
-eegs_closed_pow = np.sum(eegs_closed**2, axis=1)
-eegs_open_pow = np.sum(eegs_open**2, axis=1)
+eegs_open_pow = np.mean(eeg_open_bands**2, axis=1)
+eegs_closed_pow = np.mean(eeg_closed_bands**2, axis=1)
 
 # %% plot bar
 plt.figure()
-plt.bar(np.arange(5)+0.2,  # bar position
-        eegs_open_pow,
-        width=0.4)
-plt.bar(np.arange(5)+0.6,
-        eegs_closed_pow,
-        width=0.4)
+plt.bar(np.arange(5)+0.2, eegs_open_pow, width=0.4)
+plt.bar(np.arange(5)+0.6, eegs_closed_pow, width=0.4)
 plt.legend(['eyes open', 'eyes closed'])
-plt.xticks(np.arange(5)+0.4, 'delta theta alpha beta gamma'.split())
+plt.xticks(np.arange(5)+0.4, bands)
 
-# %% normalised power
+# %% relative power
 eegs_closed_pow_norm = eegs_closed_pow / np.sum(eegs_closed_pow)
 eegs_open_pow_norm = eegs_open_pow / np.sum(eegs_open_pow)
 
+# %% plot norm pow bar
 plt.figure()
-plt.bar(np.arange(5)+0.2,
-        eegs_open_pow_norm,
-        width=0.4)
-plt.bar(np.arange(5)+0.6,
-        eegs_closed_pow_norm,
-        width=0.4)
+plt.bar(np.arange(5)+0.2, eegs_open_pow_norm, width=0.4)
+plt.bar(np.arange(5)+0.6, eegs_closed_pow_norm, width=0.4)
 plt.legend(['eyes open', 'eyes closed'])
-plt.xticks(np.arange(5)+0.4, 'delta theta alpha beta gamma'.split())
+plt.xticks(np.arange(5)+0.4, bands)
+plt.tight_layout()
